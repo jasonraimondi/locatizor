b59efdbfba47831c7938fd80fc365a6a4a5836e5
Jason Raimondi <jason@raimondi.us>
2020-03-02 22:12:29 -0800

    Add button element

--- src/renderer/elements/Button.tsx
+++ src/renderer/elements/Button.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Button.css";
 
 type Props = any & {
   disabled?: boolean;
 };
 
 export const Button: React.FC<Props> = ({ children, ...props }) => {
-  return <button {...props} className="bg-blue-500 p-4">
+  return <button {...props} className="button">
     {children}
   </button>
 };
