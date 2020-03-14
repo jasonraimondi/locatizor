6f6f802b822a36f2f3fc500e60b483b1fe33631b
Jason Raimondi <jason@raimondi.us>
2020-03-13 22:21:52 -0700

    Add drag event to folder directory

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { DragEvent } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { parse } from "path";
 
 import { Photo } from "@/renderer/modules/photo/photo";
 import { PathListHistory } from "@/renderer/modules/path_list_history/path_list_history";
 import { Home } from "@/renderer/home";
 import { MapProvider } from "@/renderer/providers/map_provider";
+import { useCurrentPath } from "./providers/current_path";
+import { statSync } from "fs";
 
 export const App: React.FC = () => {
-  return <Router>
+  const { setCurrentPath } = useCurrentPath();
+
+  const onDragOver = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  };
+
+  const onDrop = (e: DragEvent<HTMLDivElement>) => {
+    const draggedPath = e.dataTransfer?.files?.[0]?.path;
+
+    if (draggedPath) {
+      const stats = statSync(draggedPath);
+      let dir = draggedPath;
+      if (!stats.isDirectory()) {
+        dir = parse(draggedPath).dir;
+      }
+      setCurrentPath(dir);
+    }
+  };
+
+  return <div className="h-full" onDragOver={onDragOver} onDrop={onDrop}>
+    <Router>
       <Switch>
         <Route path="/photo/:photoId">
           <Photo/>
@@ -21,5 +44,6 @@ export const App: React.FC = () => {
           </MapProvider>
         </Route>
       </Switch>
-  </Router>;
+    </Router>
+  </div>;
 };
